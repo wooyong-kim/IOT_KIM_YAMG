c06e1cd86d84a5e41a166fdd4305497e61162ff9
jby9759 <jby9759@gmail.com>
2021-05-27 15:59:00 +0900

    project 1
    1

---
+++ project/project.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+WiFiClient PJWC;
+HTTPClient PJHC;
+PubSubClient PJMC;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
